6d732416036350ad55b44397fc002ded1077ab75
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-13 11:44:46 +0530

    Jump Sort

--- app/src/main/java/com/example/jumpsortapp/presentation/jumpflow/JumpSortScreen.kt
+++ app/src/main/java/com/example/jumpsortapp/presentation/jumpflow/JumpSortScreen.kt
@@ -1,31 +1,31 @@
 package com.example.jumpsortapp.presentation.jumpflow
 
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.google.accompanist.flowlayout.FlowColumn
 
 @Composable
 fun JumpSortScreen() {
-    FlowColumn(
-        modifier = Modifier.fillMaxSize()
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
     ) {
         TopAppBar(modifier = Modifier
             .fillMaxWidth()
-            .height(100.dp)) {
+            .height(80.dp)) {
             Text(
                 text = "Jump Sort Program Calculator",
-                style = TextStyle(fontSize = 30.sp, fontWeight = FontWeight.Bold),
-                textAlign = TextAlign.Center
+                style = TextStyle(fontSize = 28.sp, fontWeight = FontWeight.Bold),
+                textAlign = TextAlign.Justify
             )
         }
     }
