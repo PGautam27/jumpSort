15decfa14b19e9d5b21a667d202977cba21961a1
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-13 11:04:48 +0530

    Jump Sort

--- app/src/main/java/com/example/jumpsortapp/presentation/screen/Screen.kt
+++ app/src/main/java/com/example/jumpsortapp/presentation/screen/Screen.kt
@@ -1,6 +1,6 @@
 package com.example.jumpsortapp.presentation.screen
 
-sealed class Screen(route : String){
+sealed class Screen(val route : String){
     object JumpSortScreen : Screen("jump_sort_screen")
     object JumpCodeScreen : Screen("jump_code_screen")
 }
