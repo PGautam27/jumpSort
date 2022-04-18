b96f204fb91a83810124c05479db8b0d3248b78a
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-18 14:22:16 +0530

    Jump Sort

--- app/src/main/java/com/example/jumpsortapp/presentation/jumpflow/JumpSortScreen.kt
+++ app/src/main/java/com/example/jumpsortapp/presentation/jumpflow/JumpSortScreen.kt
@@ -1,6 +1,7 @@
 package com.example.jumpsortapp.presentation.jumpflow
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -20,6 +21,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.jumpsortapp.ui.theme.Aquamarine
 import com.example.jumpsortapp.ui.theme.ElectricBlue
+import com.google.accompanist.flowlayout.FlowCrossAxisAlignment
+import com.google.accompanist.flowlayout.FlowRow
+import com.google.accompanist.flowlayout.MainAxisAlignment
 
 @Composable
 fun JumpSortScreen() {
@@ -36,127 +40,167 @@ fun JumpSortScreen() {
     val enabled = remember {
         mutableStateOf(true)
     }
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(verticalState)
-            .background(Aquamarine),
-    ) {
-        TopAppBar(modifier = Modifier
-            .fillMaxWidth()
-            .height(80.dp), contentPadding = PaddingValues(start = 12.dp)) {
-            Text(
-                text = "Jump Sort Program Calculator",
-                textAlign = TextAlign.Center,
-                style = TextStyle(fontSize = 28.sp, fontWeight = FontWeight.Bold),
-            )
-        }
-        Spacer(modifier = Modifier.height(15.dp))
-        Text(
-            text = "Your Input : ",
-            style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.White),
-            modifier = Modifier.padding(start = 10.dp)
-        )
-        Spacer(modifier = Modifier.height(10.dp))
-        OutlinedTextField(
-            value = inputValues.value,
-            onValueChange = { inputValues.value = it },
-            enabled = enabled.value,
-            modifier = Modifier
-                .height(90.dp)
-                .width(400.dp)
-                .padding(start = 10.dp),
-            colors = TextFieldDefaults.outlinedTextFieldColors(
-                focusedBorderColor = ElectricBlue,
-                unfocusedBorderColor = ElectricBlue,
-                cursorColor = Color.White,
-                textColor = Color.White,
-                disabledBorderColor = ElectricBlue,
-            ),
-            maxLines = 3
-            )
-        Spacer(modifier = Modifier.padding(10.dp))
-        Button(
-            onClick = { /*TODO*/ }, modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .clip(
-                    RoundedCornerShape(10.dp)
-                )
-        ) {
-            Text(text = "Insert")
-        }
-        Spacer(modifier = Modifier.padding(28.dp))
-        Row(
-            verticalAlignment = Alignment.CenterVertically,
-            horizontalArrangement = Arrangement.SpaceEvenly,
-            modifier = Modifier
+    Scaffold(
+        topBar = {
+            TopAppBar(modifier = Modifier
                 .fillMaxWidth()
-        ) {
-            Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                .height(80.dp), contentPadding = PaddingValues(start = 12.dp)) {
                 Text(
-                    text = "Enter Key : ",
-                    style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.White),
+                    text = "Jump Sort Program Calculator",
+                    textAlign = TextAlign.Center,
+                    style = TextStyle(fontSize = 28.sp, fontWeight = FontWeight.Bold),
                 )
-                Spacer(modifier = Modifier.padding(5.dp))
-                OutlinedTextField(
-                    value = keyValue.value,
-                    onValueChange = { keyValue.value = it },
-                    enabled = enabled.value,
-                    modifier = Modifier.size(50.dp),
-                    colors = TextFieldDefaults.outlinedTextFieldColors(
-                        focusedBorderColor = ElectricBlue,
-                        unfocusedBorderColor = ElectricBlue,
-                        cursorColor = Color.White,
-                        textColor = Color.White,
-                        disabledBorderColor = ElectricBlue,
-                    ),
-                    maxLines = 1
-                )
-                Spacer(modifier = Modifier.padding(10.dp))
-                Button(
-                    onClick = { /*TODO*/ }, modifier = Modifier
-                        .clip(
-                            RoundedCornerShape(10.dp)
-                        )
-                ) {
-                    Text(text = "Search")
+            }
+        },
+        bottomBar = {
+            BottomAppBar(modifier = Modifier.fillMaxWidth(), contentPadding = PaddingValues(12.dp)) {
+                FlowRow(modifier = Modifier
+                    .fillMaxSize(),
+                    mainAxisSpacing = 6.dp,
+                    crossAxisAlignment = FlowCrossAxisAlignment.Center,
+                    mainAxisAlignment = MainAxisAlignment.Center,
+                    crossAxisSpacing = 0.dp) {
+                    Column(
+                        modifier = Modifier
+                            .height(100.dp).width(125.dp)
+                            .clickable { },
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center,
+                    ) {
+                        Text(text = "Code", fontWeight = FontWeight.Bold, fontSize = 22.sp)
+                    }
+                    Column(
+                        modifier = Modifier
+                            .height(100.dp).width(125.dp)
+                            .clickable { },
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = "Code",fontWeight = FontWeight.Bold, fontSize = 22.sp)
+                    }
+                    Column(
+                        modifier = Modifier
+                            .height(100.dp).width(125.dp)
+                            .clickable { },
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Text(text = "Code",fontWeight = FontWeight.Bold, fontSize = 22.sp)
+                    }
                 }
             }
-            Column(
-                verticalArrangement = Arrangement.Top,
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier.height(140.dp)
-            ) {
-                Text(
-                    text = "Key index : ",
-                    style = TextStyle(
-                        fontSize = 20.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White
-                    ),
-                )
-                Spacer(modifier = Modifier.padding(5.dp))
-                OutlinedTextField(
-                    value = keyIndexValue.value,
-                    onValueChange = { keyIndexValue.value = it },
-                    enabled = enabled.value,
-                    modifier = Modifier.size(50.dp),
-                    colors = TextFieldDefaults.outlinedTextFieldColors(
-                        focusedBorderColor = ElectricBlue,
-                        unfocusedBorderColor = ElectricBlue,
-                        cursorColor = Color.White,
-                        textColor = Color.White,
-                        disabledBorderColor = ElectricBlue,
-                    ),
-                    maxLines = 1
-                )
-            }
         }
-        Spacer(modifier = Modifier.height(15.dp))
-        BottomAppBar(modifier = Modifier.fillMaxWidth(), contentPadding = PaddingValues(12.dp)) {
-            Button(onClick = { /*TODO*/ }) {
-
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .verticalScroll(verticalState)
+                .background(Aquamarine),
+        ) {
+            Spacer(modifier = Modifier.height(15.dp))
+            Text(
+                text = "Your Input : ",
+                style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.White),
+                modifier = Modifier.padding(start = 10.dp)
+            )
+            Spacer(modifier = Modifier.height(10.dp))
+            OutlinedTextField(
+                value = inputValues.value,
+                onValueChange = { inputValues.value = it },
+                enabled = enabled.value,
+                modifier = Modifier
+                    .height(90.dp)
+                    .width(400.dp)
+                    .padding(start = 10.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    focusedBorderColor = ElectricBlue,
+                    unfocusedBorderColor = ElectricBlue,
+                    cursorColor = Color.White,
+                    textColor = Color.White,
+                    disabledBorderColor = ElectricBlue,
+                ),
+                maxLines = 3
+            )
+            Spacer(modifier = Modifier.padding(10.dp))
+            Button(
+                onClick = { /*TODO*/ }, modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .clip(
+                        RoundedCornerShape(10.dp)
+                    )
+            ) {
+                Text(text = "Insert")
             }
+            Spacer(modifier = Modifier.padding(28.dp))
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                Column(verticalArrangement = Arrangement.Center, horizontalAlignment = Alignment.CenterHorizontally) {
+                    Text(
+                        text = "Enter Key : ",
+                        style = TextStyle(fontSize = 20.sp, fontWeight = FontWeight.Bold, color = Color.White),
+                    )
+                    Spacer(modifier = Modifier.padding(5.dp))
+                    OutlinedTextField(
+                        value = keyValue.value,
+                        onValueChange = { keyValue.value = it },
+                        enabled = enabled.value,
+                        modifier = Modifier.size(50.dp),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = ElectricBlue,
+                            unfocusedBorderColor = ElectricBlue,
+                            cursorColor = Color.White,
+                            textColor = Color.White,
+                            disabledBorderColor = ElectricBlue,
+                        ),
+                        maxLines = 1
+                    )
+                    Spacer(modifier = Modifier.padding(10.dp))
+                    Button(
+                        onClick = { /*TODO*/ }, modifier = Modifier
+                            .clip(
+                                RoundedCornerShape(10.dp)
+                            )
+                    ) {
+                        Text(text = "Search")
+                    }
+                }
+                Column(
+                    verticalArrangement = Arrangement.Top,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    modifier = Modifier.height(140.dp)
+                ) {
+                    Text(
+                        text = "Key index : ",
+                        style = TextStyle(
+                            fontSize = 20.sp,
+                            fontWeight = FontWeight.Bold,
+                            color = Color.White
+                        ),
+                    )
+                    Spacer(modifier = Modifier.padding(5.dp))
+                    OutlinedTextField(
+                        value = keyIndexValue.value,
+                        onValueChange = { keyIndexValue.value = it },
+                        enabled = enabled.value,
+                        modifier = Modifier.size(50.dp),
+                        colors = TextFieldDefaults.outlinedTextFieldColors(
+                            focusedBorderColor = ElectricBlue,
+                            unfocusedBorderColor = ElectricBlue,
+                            cursorColor = Color.White,
+                            textColor = Color.White,
+                            disabledBorderColor = ElectricBlue,
+                        ),
+                        maxLines = 1
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.height(15.dp))
+
         }
     }
+
 }
