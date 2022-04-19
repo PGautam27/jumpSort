db98e1654f9a32b16ca678fb5a8e79fb2dbcb1e3
PGautam27 <20010115510.pgautam@cse.reva.edu.in>
2022-04-19 06:16:02 +0530

    Jump Sort

--- app/src/main/java/com/example/jumpsortapp/presentation/jumpcode/JumpCodeScreen.kt
+++ app/src/main/java/com/example/jumpsortapp/presentation/jumpcode/JumpCodeScreen.kt
@@ -1,13 +1,20 @@
 package com.example.jumpsortapp.presentation.jumpcode
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 @Composable
@@ -23,13 +30,19 @@ fun JumpCodeScreen() {
                 },
                 navigationIcon = {
                     IconButton(onClick = {  }) {
-                        Icon(Icons.Filled.ArrowBack, "backIcon")
+                        Icon(Icons.Filled.ArrowBack, "backIcon", modifier = Modifier.size(20.dp))
                     }
                 },
                 contentColor = Color.White,
             )
     }
     ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.fillMaxSize()
+        ) {
 
+        }
     }
 }
